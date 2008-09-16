f7c3cf80658b61a445828f1cf62468a22d87a485
brian <brian@92d6f99d-2424-4bb1-b2f5-f90525c0f07d>
2008-09-16 21:18:22 +0000

    Updates to the engine deployment routine.
    git-svn-id: https://svn.plectix.com/plectix/plx-engine/trunk@2472 92d6f99d-2424-4bb1-b2f5-f90525c0f07d

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,29 @@
+#
 # Deploy recipe for plx_engines
+#
 
+namespace :release do
+  namespace :plx_engine do
+
+    # Kickstart for releasing plx-engine
+    task :default do
+      release.plx_engine.compile
+    end
+    # compilation
+    task :compile, :roles => :job do
+      run "cd /var/app/plx_engine/current; make;"
+    end
+
+  end
+end
+
+
+namespace :deploy do
+  namespace :plx_engine do
+
+    # Kickstart for deploying plx-engine
+    task :default do
+    end
+
+  end
+end
