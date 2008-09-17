7c7e862a367ca59d69a2049181793e2dcd61ce79
brian <brian@92d6f99d-2424-4bb1-b2f5-f90525c0f07d>
2008-09-17 20:53:03 +0000

    Working deployment routine.
    git-svn-id: https://svn.plectix.com/plectix/plx-engine/trunk@2504 92d6f99d-2424-4bb1-b2f5-f90525c0f07d

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,11 +7,20 @@ namespace :release do
 
     # Kickstart for releasing plx-engine
     task :default do
+      release.plx_engine.update_code
       release.plx_engine.compile
     end
     # compilation
     task :compile, :roles => :job do
       run "cd /var/app/plx_engine/current; make;"
+    end
+
+    task :update_code do
+      set :repository, "https://svn.plectix.com/plectix/plx-engine/#{repository_path}"
+      set :application, "plx_engine"
+      release.reset_paths
+      run "svn checkout --username #{scm_username} --password #{scm_password} --no-auth-cache #{repository} #{release_path}"
+      run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}"
     end
 
   end
