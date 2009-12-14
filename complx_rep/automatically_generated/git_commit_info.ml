ea6b9a7b3499ef1f38d39c3b88d15fb862141dca
jerome <jerome.feret@plectix.com>
2009-12-14 12:04:12 +0100

    Propagating git version into simplx.

--- complx_rep/automatically_generated/git_commit_info.ml
+++ complx_rep/automatically_generated/git_commit_info.ml
@@ -1,1 +1,1 @@
-let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 4 ,321 ,10501 ,"2009-12-14 12:02:43 "
+let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 4 ,321 ,10502 ,"2009-12-14 12:04:12 "
