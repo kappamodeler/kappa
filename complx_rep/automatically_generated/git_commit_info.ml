30431d22d0180d326eb54466d76cc0a915eaca95
Brian Kaney <brian@vermonster.com>
2009-12-14 17:59:33 -0500

    Release with S3 (hopefully)

--- complx_rep/automatically_generated/git_commit_info.ml
+++ complx_rep/automatically_generated/git_commit_info.ml
@@ -1,1 +1,1 @@
-let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 5 ,2,10505 ,"2009-12-14 17:53:19 "
+let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 5 ,3,10506 ,"2009-12-14 17:59:33 "
