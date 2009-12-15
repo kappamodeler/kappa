7106f31f8bd70968eb5d9de5e6665972c4e56983
jerome <jerome.feret@plectix.com>
2009-12-15 09:27:49 +0100

    Test Tags.

--- complx_rep/automatically_generated/git_commit_info.ml
+++ complx_rep/automatically_generated/git_commit_info.ml
@@ -1,1 +1,1 @@
-let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 5 ,4 ,2 ,"2009-12-15 09:26:52 "
+let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 6 ,1 ,3 ,"2009-12-15 09:27:49 "
