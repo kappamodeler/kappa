91e074f1a2ba3bedfe385821df2bc182c7462e7d
Jean Krivine <jkrivine@molybdene.pps.jussieu.fr>
2010-02-09 15:55:15 +0100

    cleanup file missing

--- complx_rep/automatically_generated/git_commit_info.ml
+++ complx_rep/automatically_generated/git_commit_info.ml
@@ -1,1 +1,1 @@
-let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 4,321,10527,"2010-02-08 15:08:50"
+-n let git_commit_version,git_commit_release,git_commit_tag,git_commit_date = 4,321,-n 10528,"2010-02-09 15:55:15"
